25db3ccf45aa8b84ea528202a22dac4251c30c14
Ruanito Santos <rds11@c3sl.ufpr.br>
2014-10-28 18:25:40 -0200

    Adicionado lista

--- gen/br/ufpr/ci317wifi/R.java
+++ gen/br/ufpr/ci317wifi/R.java
@@ -97,5 +97,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int wifiList=0x7f060002;
     }
 }
