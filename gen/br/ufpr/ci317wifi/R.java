5c47513215a7d50d762403e39d145da20e42b6e0
Ruanito Santos <rds11@c3sl.ufpr.br>
2014-11-04 14:51:58 -0200

    Adicionaodo layout main para varios tipos de telas

--- gen/br/ufpr/ci317wifi/R.java
+++ gen/br/ufpr/ci317wifi/R.java
@@ -24,6 +24,7 @@ public final class R {
     public static final class drawable {
         public static final int hero_airport_wifi=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int wifi_logo=0x7f020002;
     }
     public static final class id {
         public static final int action_info=0x7f080013;
