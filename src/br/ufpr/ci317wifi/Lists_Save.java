36f5316f1b0e59921b5d2969ecd33485772099a5
Alessandro Elias <ae11@c3sl.ufpr.br>
2014-11-17 02:27:18 -0200

    Show list of wifi's known.

--- src/br/ufpr/ci317wifi/Lists_Save.java
+++ src/br/ufpr/ci317wifi/Lists_Save.java
@@ -1,21 +1,50 @@
 package br.ufpr.ci317wifi;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Lists_Save extends Activity {
-	
+	WifiManager wifiManager;
 	Intent intent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lists__save);
+		
+		wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
 	}
 
+	@Override
+	public void onResume(){
+		List<WifiConfiguration> wConfigs = wifiManager.getConfiguredNetworks();
+		String[] ssids = new String[wConfigs.size()];
+		
+		int i = 0;
+		for( WifiConfiguration wc : wConfigs ) {
+		   /*
+    		* WARNING: this is a workaround since getConfiguredNetworks()
+    		*          fill up the name in double quotes.
+    		*/
+			ssids[i++] = wc.SSID.replaceAll("\"", "");
+		}
+		
+		ListView lv = (ListView)findViewById(R.id.wifiListSave);
+		lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, ssids));
+		
+		super.onResume();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
