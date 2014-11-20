4bc12edf67effc57f49e7e6bcb54c3bda186c0d1
Alessandro <alessandro@dexter.c3local>
2014-11-20 10:54:37 -0200

    Defining a default for layouts.

--- src/br/ufpr/ci317wifi/Info.java
+++ src/br/ufpr/ci317wifi/Info.java
@@ -28,6 +28,7 @@ public class Info extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+		@SuppressWarnings("unused")
 		int id = item.getItemId();
 		
 		return super.onOptionsItemSelected(item);
